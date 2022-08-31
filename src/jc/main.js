71c1c1646ca06986327f4d5ef4f2505bb70aaf5d
albina <nuralievaalbina0@gmail.com>
2022-08-31 19:40:54 +0600

    first commit

---
+++ src/jc/main.js
@@ -0,0 +1,8 @@
+$('.mobile-btn').on('click', function (){
+  $('.header__menu').toggleClass('show-menu');
+});
+
+
+$('.menu a nav').on('click', function (){
+  $('.menu').removeClass('show-menu');
+});
